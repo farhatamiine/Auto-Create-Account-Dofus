6b706fff59493bbc3ce5e2b49e6a6df60d93aaa3
farhatamiine <farhatamiine@gmail.com>
2021-03-31 14:07:06 +0100

    open browser to the confirmed link

--- confirmMail.js
+++ confirmMail.js
@@ -1,24 +1,29 @@
 const axios = require('axios').default
 const chalk = require('chalk')
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 const cheerio = require('cheerio')
-var fs = require('fs')
+const fs = require('fs')
+const delay = require('./utils/function')
 
 const userCredential = {
     address: 'aminefarhat@maxresistance.com',
     password: 'amine.farhat50',
 }
 
+puppeteer.use(StealthPlugin())
+
 async function getJwtToken(userCredentials) {
-    return await axios.post('https://api.mail.tm/token', {
+    return axios.post('https://api.mail.tm/token', {
         address: userCredentials.address,
         password: userCredentials.password,
     })
 }
 
 async function getAllMessages() {
-    let rawdata = fs.readFileSync('token.json')
-    let token = JSON.parse(rawdata)
-    return await axios.get('https://api.mail.tm/messages?page=1', {
+    const rawdata = fs.readFileSync('token.json')
+    const token = JSON.parse(rawdata)
+    return axios.get('https://api.mail.tm/messages?page=1', {
         headers: {
             Authorization: `Bearer ${token.token}`,
             accept: 'application/ld+json',
@@ -26,36 +31,45 @@ async function getAllMessages() {
     })
 }
 
-getJwtToken(userCredential).then((res) => {
-    let data = JSON.stringify(res.data)
-    fs.writeFileSync('token.json', data)
-})
+const confirmMail = async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 10,
+        defaultViewPort: null,
+    })
+    const page = await browser.newPage()
+    await page.setDefaultNavigationTimeout(0)
 
-getAllMessages().then((res) => {
-    const message = res.data['hydra:member'].filter((e) => {
-        return (
-            e.subject == 'Merci de confirmer votre email pour jouer à DOFUS' &&
-            e.seen == true
-        )
+    getJwtToken(userCredential).then((res) => {
+        const data = JSON.stringify(res.data)
+        fs.writeFileSync('token.json', data)
     })
-    let rawdata = fs.readFileSync('token.json')
-    let token = JSON.parse(rawdata)
-    const email = axios
-        .get(`https://api.mail.tm${message[0]['@id']}`, {
-            headers: {
-                Authorization: `Bearer ${token.token}`,
-                accept: 'application/ld+json',
-            },
+
+    getAllMessages().then((res) => {
+        const message = res.data['hydra:member'].filter((e) => (
+            e.subject === 'Merci de confirmer votre email pour jouer à DOFUS' &&
+            e.seen === true
+        ))
+        const rawdata = fs.readFileSync('token.json')
+        const token = JSON.parse(rawdata)
+        const email = axios
+            .get(`https://api.mail.tm${message[0]['@id']}`, {
+                headers: {
+                    Authorization: `Bearer ${token.token}`,
+                    accept: 'application/ld+json',
+                },
+            })
+            .then((response) => response.data)
+        email.then(async (result) => {
+            console.log(chalk.green("Confirming the created account!!"));
+            const $ = cheerio.load(result.html[0])
+            const confirmationLink = $('td>a').first().attr('href')
+            await page.goto(confirmationLink)
+            await delay(3000)
+            browser.close();
         })
-        .then((res) => {
-            return res.data
-        })
-    email.then((res) => {
-        //console.log(res.html[0])
-        const $ = cheerio.load(res.html[0])
-        const confirmationLink = $('td>a').first().attr('href')
-        console.log(
-            `${chalk.green('📩 Confirmation link :')} ${confirmationLink}`
-        )
     })
-})
+}
+
+
+confirmMail();
